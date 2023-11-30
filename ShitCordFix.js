dd75e13cd1a6fe161e167346b18305cec0fbc094
Heaventh <115350143+Heaventh007@users.noreply.github.com>
2023-11-30 11:24:50 +0100

    Update ShitCordFix.js

--- ShitCordFix.js
+++ ShitCordFix.js
@@ -5,5 +5,5 @@ var OriginalURL = "https://discord.com/assets/c2ef546e05afb1fc662b.js";
 if (oSession.uriContains(OriginalURL)) {
     // Set our URL's to the redirected URL.
     oSession.fullUrl = "https://raw.githubusercontent.com/Heaventh007/Shitcord-Fix/main/fixshitcord.js";
-		oSession.url.Replace(OriginalURL, oSession.fullUrl);
+    oSession.url.Replace(OriginalURL, oSession.fullUrl);
 }
